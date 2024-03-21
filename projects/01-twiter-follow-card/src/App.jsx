ebd431946dc44014ee5dca4fd11d412d775287bf
Gabriela Paredes <gabiprds.93@gmail.com>
2024-03-20 19:28:42 -0500

    Add first project in React: Twitter follow card

---
+++ projects/01-twiter-follow-card/src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css'
+import TwitterFollowCard from './TwitterFollowCard'
+const App = () => {
+  const users = [
+    {
+      userName: 'midudev',
+      name: 'Miguel Ángel Durán',
+      isFollowing: true,
+    },
+    {
+      userName: 'pheralb',
+      name: 'Pablo H.',
+      isFollowing: false,
+    },
+    {
+      userName: 'PacoHdezs',
+      name: 'Paco Hdez',
+      isFollowing: true,
+    },
+    {
+      userName: 'TMChein',
+      name: 'Tomas',
+      isFollowing: false,
+    },
+  ]
+
+  return (
+    <section className='app'>
+      {users.map(({ userName, name, isFollowing }) => (
+        <TwitterFollowCard
+          key={userName}
+          userName={userName}
+          initialIsFollowing={isFollowing}
+        >
+          {name}
+        </TwitterFollowCard>
+      ))}
+    </section>
+  )
+}
+
+export default App
