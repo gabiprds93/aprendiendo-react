7b2c3035a41a4c5204756de75f6b05b6cacfbdb6
Gabriela Paredes <gabiprds.93@gmail.com>
2024-03-26 17:55:59 -0500

    Extract to component Tic tac toe

---
+++ projects/02-tic-tac-toe/src/components/WinnerModal.jsx
@@ -0,0 +1,20 @@
+import { Square } from './Square'
+
+export function WinnerModal({ resetGame, winner }) {
+  if (winner === null) return null
+
+  const winnerText = winner ? 'Ganó' : 'Empate'
+
+  return (
+    <section className='winner'>
+      <div className='text'>
+        <h2>{winnerText}</h2>
+        <header className='win'>{winner && <Square>{winner}</Square>}</header>
+
+        <footer>
+          <button onClick={resetGame}>Empezar de nuevo</button>
+        </footer>
+      </div>
+    </section>
+  )
+}
