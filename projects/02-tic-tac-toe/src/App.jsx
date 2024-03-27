568a907134aec474e6b2601509094be08dbe24b0
Gabriela Paredes <gabiprds.93@gmail.com>
2024-03-27 18:13:20 -0500

    Refactor functions, improve components

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -3,12 +3,19 @@ import confetti from 'canvas-confetti'
 
 import { Square } from './components/Square'
 import { TURNS } from './constants'
-import { checkEndGame, checkWinner } from './utils'
+import { checkEndGame, checkWinner } from './utils/board'
 import { WinnerModal } from './components/WinnerModal'
+import { resetGameStorage, saveGameStorage } from './utils/storage'
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
   const [winner, setWinner] = useState(null)
 
   const updateBoard = (index) => {
@@ -18,6 +25,7 @@ function App() {
     setBoard(newBoard)
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    saveGameStorage({ board: newBoard, turn: newTurn })
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
       confetti()
@@ -29,11 +37,15 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+    resetGameStorage()
   }
 
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
+
+      <button onClick={resetGame}>Reset del juego</button>
+
       <section className='game'>
         {board.map((square, index) => {
           return (
@@ -46,6 +58,7 @@ function App() {
 
       <section className='turn'>
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
 
--- projects/02-tic-tac-toe/src/utils/board.js
+++ projects/02-tic-tac-toe/src/utils/board.js
@@ -1,8 +1,9 @@
-import { WINNER_COMBOS } from './constants'
+import { WINNER_COMBOS } from '../constants'
 
 export const checkWinner = (boardToCheck) => {
   for (const combo of WINNER_COMBOS) {
     const [a, b, c] = combo
+
     if (
       boardToCheck[a] &&
       boardToCheck[a] === boardToCheck[b] &&
